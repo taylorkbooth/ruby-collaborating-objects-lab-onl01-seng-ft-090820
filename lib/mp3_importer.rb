c9dd9727cd02d34256a12cfa48a8da3312841dfe
Taylor Booth <taylorkbooth@gmail.com>
2020-09-25 16:35:58 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,24 @@
+class MP3Importer
+  
+  attr_accessor :path 
+  
+  def initialize(filepath)
+    @path = filepath
+  end
+  
+  def files
+    @files = Dir.entries(@path)
+    @files.delete_if {|file| file == "." || file == ".."}
+  end
+
+
+  def import
+    self.files.each do |filename|
+      Song.new_by_filename(filename)
+    end
+  end
+  
+  
+  
+  
+end
